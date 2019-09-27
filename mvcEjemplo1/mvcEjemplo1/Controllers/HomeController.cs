a367b0b538991fa7314a8b638c83679e3290aaa3
Geovani de Leon <erwingeo1021@gmail.com>
2019-09-27 11:30:31 -0600

    Uso de ViewBag entre Controlador -> Vista

--- mvcEjemplo1/mvcEjemplo1/Controllers/HomeController.cs
+++ mvcEjemplo1/mvcEjemplo1/Controllers/HomeController.cs
@@ -30,6 +30,20 @@ namespace mvcEjemplo1.Controllers
         public ActionResult Hola()
         {
             ViewBag.Message = "Prueba";
+            string[] lista = new String[10];
+            lista[1] = "Hola 1";
+            lista[2] = "Hola 2";
+            lista[3] = "Hola 3";
+            lista[4] = "Hola 4";
+
+            List<String> lstString = new List<String>();
+            lstString.Add("Hola mundo 1");
+            lstString.Add("Hola mundo 2");
+            lstString.Add("Hola mundo 3");
+            lstString.Add(" ");
+
+            ViewBag.lista = lista;
+            ViewBag.lista2 = lstString;
 
             return View();
         }
